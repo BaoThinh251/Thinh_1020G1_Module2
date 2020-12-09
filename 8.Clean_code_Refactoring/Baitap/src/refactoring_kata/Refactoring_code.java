fdf16d4ace1b5de144fbb5924a6b50fb70b88548
C1020G1_BảoThịnh <baothinhtran159@gmail.com>
2020-12-09 17:20:10 +0700

    baitap

--- 8.Clean_code_Refactoring/Baitap/src/refactoring_kata/Refactoring_code.java
+++ 8.Clean_code_Refactoring/Baitap/src/refactoring_kata/Refactoring_code.java
@@ -1,27 +1,66 @@
 package refactoring_kata;
 
 public class Refactoring_code {
-   public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
-       String score = "";
-       int tempScore = 0;
-       if (m_score1 == m_score2)
-       {
-           switch (m_score1)
-           {
-               case 0:
+    public static String getScore( int player1Score, int player2Score) {
+        String score = "";
+        int tempScore;
+        if (player1Score == player2Score)
+        {
+            switch (player1Score)
+            {
+                case 0:
+                    score = "Love";
+                    break;
+                case 1:
+                    score = "Fifteen";
+                    break;
+                case 2:
+                    score = "Thirty";
+                    break;
+                case 3:
+                    score = "Forty";
+                    break;
+                default:
+                    score = "Deuce";
+                    break;
 
-                   break;
-               case 1:
-                   break;
-               case 2:
-                   break;
-               case 3:
-                   break;
-               default:
-                   break;
-
-           }
-       }
-       return player1Name;
-   }
+            }
+        }
+        else if (player1Score >=4 || player2Score >= 4) {
+            int minusResult = player1Score - player2Score;
+            if (minusResult == 1) {
+                score = "Advantage player1";
+            } else if (minusResult == -1) {
+                score = "Advantage player2";
+            } else if (minusResult >= 2) {
+                score = "Win for player1";
+            } else {
+                score = "Win for player2";
+            }
+        }
+        else
+        {
+            for (int i = 1; i < 3; i++)
+            {
+                if (i == 1) tempScore = player1Score;
+                else { score = "-"; tempScore = player2Score;}
+                switch(tempScore)
+                {
+                    case 0:
+                        score = "Love";
+                        break;
+                    case 1:
+                        score = "Fifteen";
+                        break;
+                    case 2:
+                        score = "Thirty";
+                        break;
+                    case 3:
+                        score = "Forty";
+                        break;
+                }
+            }
+        }
+        return score;
+    }
 }
