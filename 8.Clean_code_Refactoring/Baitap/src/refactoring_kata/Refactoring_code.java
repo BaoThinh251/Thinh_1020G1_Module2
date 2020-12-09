6debe68d2430c4d9f5462ccf33feb8c25ba27749
C1020G1_BảoThịnh <baothinhtran159@gmail.com>
2020-12-09 16:57:32 +0700

    thuchanh

--- 8.Clean_code_Refactoring/Baitap/src/refactoring_kata/Refactoring_code.java
+++ 8.Clean_code_Refactoring/Baitap/src/refactoring_kata/Refactoring_code.java
@@ -1,4 +1,27 @@
 package refactoring_kata;
 
 public class Refactoring_code {
+   public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
+       String score = "";
+       int tempScore = 0;
+       if (m_score1 == m_score2)
+       {
+           switch (m_score1)
+           {
+               case 0:
+
+                   break;
+               case 1:
+                   break;
+               case 2:
+                   break;
+               case 3:
+                   break;
+               default:
+                   break;
+
+           }
+       }
+       return player1Name;
+   }
 }
